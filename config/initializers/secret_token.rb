7ab0d72fa78c7f8e6851bcfb5560de3d36969d06
Joon Lee <seouri@gmail.com>
2011-06-16 14:16:56 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cemu::Application.config.secret_token = '5201134c3116b597c05442c5d0dc00da7cffeead087f186e6c10bab2b1951953a448dedaf1bf07bac8e4708648f9cd4662c1a7456c75b6f5f4f08f5a6b5d5f72'
