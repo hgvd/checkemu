e5110a734b33cee6f797ecb3237bb799e82d20dc
Joon Lee <seouri@gmail.com>
2011-06-21 15:23:15 -0400

    show article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,8 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.order(:id).page(params[:page])
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
 end
